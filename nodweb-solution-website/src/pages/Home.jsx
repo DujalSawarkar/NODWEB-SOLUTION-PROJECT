51036a6b4b59efe785da41c0130fdc41008c9b5a
prince-kr-dev <princekumark2004@gmail.com>
2026-01-22 23:42:46 +0530

    home page completed with all sections

--- nodweb-solution-website/src/pages/Home.jsx
+++ nodweb-solution-website/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Hero from "../sections/Hero";
 import ServiceOverview from "../sections/ServiceOverview";
+import WhyNodWeb from "../sections/WhyNodWeb";
+import OurProcess from "../sections/OurProcess";
+import Technologies from "../sections/Technologies";
+import CallToAction from "../sections/CallToAction";
 
 function Home() {
   return(
     <>
     <Hero/>
     <ServiceOverview/>
+    <WhyNodWeb/>
+    <OurProcess/>
+    <Technologies/>
+    <CallToAction/>
     </>
   );
 }
