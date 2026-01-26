876883bbdf0862a7faef737df772490f8cf9674b
prince-kr-dev <princekumark2004@gmail.com>
2026-01-26 10:16:55 +0530

    added smooth scroll to top

--- nodweb-solution-website/src/App.jsx
+++ nodweb-solution-website/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
-import Loader from "./components/Loader"
+import Loader from "./components/Loader";
+import ScrollToTop from "./components/ScrollToTop";
 
 const Home = React.lazy(() => import("./pages/Home"));
 const About = React.lazy(() => import("./pages/About"));
@@ -11,17 +12,20 @@ const Contact = React.lazy(() => import("./pages/Contact"));
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="services" element={<Services />} />
-          <Route path="career" element={<Careers />} />
-          <Route path="contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </Suspense>
+    <>
+      <ScrollToTop />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="career" element={<Careers />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
