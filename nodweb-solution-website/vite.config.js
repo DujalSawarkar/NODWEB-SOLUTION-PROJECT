6d0d9b89de496fd805374f9b84b2115bd0de4d15
prajwal narnaware <prajwalnarnaware91@gmail.com>
2026-02-18 15:54:13 +0530

    ADDED

--- nodweb-solution-website/vite.config.js
+++ nodweb-solution-website/vite.config.js
@@ -5,11 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
+    // Use the standard react plugin without the custom babel plugin.
+    // The `babel-plugin-react-compiler` tries to import a non-exported
+    // entry from React (./compiler-runtime) and causes Vite to fail.
+    react(),
     tailwindcss(),
   ],
 });
